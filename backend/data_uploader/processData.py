55a4fde6231c985db3f58060e0de33e040a5dfd3
mitchos <mbo2@rice.edu>
2022-11-20 19:52:30 -0600

    fortschritt 8

--- backend/data_uploader/processData.py
+++ backend/data_uploader/processData.py
@@ -4,10 +4,9 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, collect_list, array_contains
 from pyspark.sql.types import StructField, IntegerType, StructType, StringType
-
+import uuid
+from pyspark.sql.functions import udf, lit
 spark = SparkSession.builder.getOrCreate()
-
-# sqlContext = SparkSession.builder.enableHiveSupport().getOrCreate()
 
 schema = StructType([
     StructField("translation_index_1", IntegerType(), True),
@@ -18,9 +17,16 @@ schema = StructType([
 
 df = spark.read.csv("deu_eng.tsv", header=False, schema=schema, sep="\t").drop(*["translation_index_1", "translation_index_2"])
 groupedBy = df.groupby("original_text").agg(collect_list('translated_text'))
+groupedBy = groupedBy.withColumnRenamed("collect_list(translated_text)", "eng_translation")
+uuidUdf = udf(lambda: str(uuid.uuid4()), StringType())
+# groupedBy = groupedBy.withColumn("id", uuidUdf())
+groupedBy = groupedBy.withColumns({"id": uuidUdf(), "language": lit("deu-eng")})
+# groupedBy = groupedBy.withColumn("language", lit("deu-eng"))
 groupedBy.printSchema()
 groupedBy.filter(groupedBy.original_text == "Was ist das?").show(truncate=False)
 groupedBy.toPandas().to_json("./groupedByTranslations", orient='records')
 # groupedBy.filter(array_contains("collect_list(translated_text)", "What is that?")).show(truncate=False)
 # df.printSchema()
 # df.show()
+
+# primary key, secondary key
